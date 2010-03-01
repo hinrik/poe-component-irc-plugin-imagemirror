9c112760fe4b8e85ab515bdd6abd193caa6e1b6d
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2010-03-01 00:16:18 +0000

    Bump version to 0.04

--- lib/POE/Component/IRC/Plugin/ImageMirror.pm
+++ lib/POE/Component/IRC/Plugin/ImageMirror.pm
@@ -11,7 +11,7 @@ use POE::Component::IRC::Plugin qw(PCI_EAT_NONE PCI_EAT_PLUGIN);
 use POE::Component::IRC::Plugin::URI::Find;
 use POE::Wheel::Run;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 my $uri_title_code = <<'END';
 use strict;
